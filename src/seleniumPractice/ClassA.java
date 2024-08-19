8caaeda79eaff3d154e25b16c5c183d5a34ef477
ayushikushwah <ayushikushwah567@gmail.com>
2024-08-19 15:41:20 +0530

    [Ayushi]Changes added in ClassA.

--- src/seleniumPractice/ClassA.java
+++ src/seleniumPractice/ClassA.java
@@ -5,6 +5,11 @@ public class ClassA {
 	public static void main(String[] args) {
 
 		System.out.println("this is github A code");
+		
+		int a = 10;
+		int b =20;
+		int c = a+b;
+		System.out.println("Addition : "+ c);
 	}
 
 }
